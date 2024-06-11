bc72e179f0ed9cf11208133ab723fc2f8421706b
Kyle Blakley <99106018+Kblakley21@users.noreply.github.com>
2024-06-11 03:43:10 -0700

    Uploaded calculator project
    The Javascript is already working to give the calculator function. Styling still being tweaked

---
+++ Calculator/index.js
@@ -0,0 +1,20 @@
+const onScreen = document.getElementById('screen');
+
+
+
+function pushToScreen(input) {
+    onScreen.value += input;
+}
+
+function clearScreen() {
+    onScreen.value = ''
+}
+
+function calculate() {
+    try {
+        onScreen.value = eval(onScreen.value)
+        }
+        catch(error) {
+            onScreen.value = 'error';
+        }
+    }
