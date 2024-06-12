2d922a5613391c155fe3e40bf1e6b25bec200c75
Kyle Blakley <kblakley2121@gmail.com>
2024-06-12 01:48:03 -0700

    Started Paperclip Game Project
    This is a version of the game Universal Paperclips by Frank Lantz, and all credit for the idea goes to him. I learned about this game and thought it would be a great test of my skills.
    **Initial Commit: Laid out the basic HTML and created two functions. addClips is working as expected; buyWire is still bugged and will not update HTML elements yet.

---
+++ Paperclips/index.js
@@ -0,0 +1,43 @@
+let currentClips = 0
+let currentCash = 5.0
+let wireStock = 20;
+let outOfWire = document.getElementById('failed-to-make').innerText = 'Out of Wire!'
+let outOfCash = document.getElementById('failed-to-buy').innerHTML = "You're out of money!"
+//inital variable testing
+console.log(currentClips)
+console.log(currentCash)
+console.log(wireStock)
+console.log(outOfWire)
+
+
+//add 1 to currentClips when button onclick occurs
+function addClips() {
+
+    if (wireStock > 4) {
+        currentClips++;
+        document.getElementById('clip-counter').innerHTML = currentClips
+        console.log(currentClips);
+        wireStock -= 4
+        document.getElementById('wire-stock').innerText = wireStock
+        console.log(wireStock)
+    } else {
+        const deniedMake = 'Not enough wire!'
+        document.getElementById('failed-to-make').innerText = deniedMake
+        console.log('not enough wire!')
+    }
+
+    function buyWire() {
+        if (currentCash >= .10) {
+            wireStock++
+            Math.floor(currentCash -= .10)
+            document.getElementById('wire-stock'.innerText) == wireStock
+            document.getElementById('current-cash'.innerText) == currentCash
+        } else {
+            document.getElementById('failed-to-buy'.innerText) == outOfCash
+
+            console.log('Out of cash!')
+        }
+        console.log(wireStock)
+        console.log(currentCash)
+    }
+}
